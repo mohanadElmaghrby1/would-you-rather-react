d2bbc33f4a9a64255774ee1b559c4455b79fe46d
mohannad <mohanad2021996@gmail.com>
2021-05-05 02:21:43 +0200

    Add Taps

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
-import React,{ Component } from "react";
+import React, { Component, Fragment } from "react";
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
+import Login from './Login'
+class Home extends Component {
 
-class Home extends Component{
-
-    render(){
+    render() {
         return (
-            <div>Home</div>
+            <Fragment>
+                <Tabs>
+                    <Tab eventKey="unanswered" title="Unanswered Questions">
+                       {/* <div>hi</div> */}
+                       <Login/>
+                    </Tab>
+                    <Tab eventKey="answered" title="Answered Questions">
+                       <div>hi</div>
+                        
+                    </Tab>
+                </Tabs>
+            </Fragment>
         )
     }
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,15 +20,8 @@ class Login extends Component {
                         <Nav className="mr-auto">
 
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#home">New Question</Nav.Link>
+                            <Nav.Link href="#new">New Question</Nav.Link>
                             <Nav.Link href="#link">Leaderboard</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown" className="mr-sm-2">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
                         </Nav>
                         <Navbar.Toggle />
                         <Navbar.Collapse className="justify-content-end">
