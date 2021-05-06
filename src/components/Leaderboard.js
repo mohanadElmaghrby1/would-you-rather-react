997ba71fcfe2f8b98307c2276f19a2dbe3374e3c
mohannad <mohanad2021996@gmail.com>
2021-05-06 10:54:52 +0200

    Sort users by score

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,8 +5,7 @@ import { Row } from 'react-bootstrap'
 
 class Leaderboard extends Component {
     render() {
-        const { users } = this.props
-        const userIds = Object.keys(users)
+        const { users, userIds } = this.props
         return (
             <div className='justify-content-center mt-4'>
                 <h3>LeaderBoard</h3>
@@ -23,12 +22,30 @@ class Leaderboard extends Component {
         )
     }
 }
-
+/**
+ * 
+ * @param {sarahedo: {
+      id: 'sarahedo',
+      name: 'Sarah Edo',
+      avatarURL: 'https://tylermcginnis.com/would-you-rather/sarah.jpg',
+      answers: {
+        "8xf0y6ziyjabvozdd253nd": 'optionOne',
+        "6ni6ok3ym7mf1p33lnez": 'optionTwo',
+        "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+        "loxhs1bqm25b708cmbf3g": 'optionTwo'
+      },
+      questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+    }} param0 
+ */
 
 
 function mapeStateToProps({ users }) {
+    const userIds = Object.keys(users)
+                .sort((a, b) => (users[b].questions.length+Object.keys(users[b].answers).length)
+                 - (users[a].questions.length+Object.keys(users[a].answers).length));
     return {
-        users
+        users,
+        userIds
     }
 }
 export default connect(mapeStateToProps)(Leaderboard)
